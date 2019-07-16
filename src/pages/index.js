1aff7369116c07cc3318a7e557de461e947a4613
Simon Adcock <simonadcock2@gmail.com>
2019-07-16 15:59:12 +0100

    first attempt at IA

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,48 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Source: Pattern Library" />
-    <h2>Pattern library</h2>
+    <section>
+      <h2>Pattern library</h2>
+      <p>Values that are used within our patterns.</p>
+      <h3>Tokens</h3>
+      <p>
+        <a href="https://github.com/guardian/dotcom-rendering/tree/master/packages/pasteup">
+          Pasteup
+        </a>
+      </p>
+      <ul>
+        <li>Colour</li>
+        <li>Spacing</li>
+        <li>Typography</li>
+        <li>Z-index</li>
+        <li>Tabindex</li>
+        <li>Icons</li>
+        <li>Animation</li>
+      </ul>
+    </section>
+    <section>
+      <h3>Utilities</h3>
+      <p>
+        Utility mixins and functions that perform some calculation for use with
+        our styles
+      </p>
+    </section>
+    <section>
+      <h3>Patterns</h3>
+      <p>Visual units that represent behaviour or enable a user action</p>
+      <ul>
+        <li>
+          <h3>Dropdown</h3>
+          <h4>Purpose</h4>
+          <p>
+            Dropdowns expand to reveal a menu of options. They close themselves
+            when the user clicks out of them.
+          </p>
+          <h4>Example</h4>
+          <h4>Variants</h4>
+        </li>
+      </ul>
+    </section>
   </Layout>
 )
 
